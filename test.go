9b59770546f3d9aa30649d6cc58f7fc8ed36aceb
Tony Spataro <tony@rightscale.com>
2017-02-06 21:11:38 -0800

    Expose TestKey for convenience.

--- test.go
+++ test.go
@@ -2,18 +2,18 @@ package jwtauth
 
 import "github.com/goadesign/goa"
 
-const testKey = "https://github.com/rightscale/goa-jwtauth#test"
+const TestKey = "https://github.com/rightscale/goa-jwtauth#test"
 
 // TestMiddleware returns a middleware that uses a static HMAC key and is
 // suitable for unit tests.
 func TestMiddleware(scheme *goa.JWTSecurity) goa.Middleware {
-	return New(scheme, &SimpleKeystore{Key: []byte(testKey)})
+	return New(scheme, &SimpleKeystore{Key: []byte(TestKey)})
 }
 
 // TestToken creates a JWT with the specified claims and signs it using
 // the same static HMAC key used by TestMiddleware().
 func TestToken(keyvals ...interface{}) string {
-	key := []byte(testKey)
+	key := []byte(TestKey)
 	token, err := NewToken(key, NewClaims(keyvals...))
 	if err != nil {
 		panic(err)
